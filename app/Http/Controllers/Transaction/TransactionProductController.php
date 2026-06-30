e5aafd6f063acc1a07f752e9b1f3d18757d122c3
Bagus Slamet Oetomo <bagus.oetomo11@gmail.com>
2026-06-30 11:10:47 +0700

    update ims

--- app/Http/Controllers/Transaction/TransactionProductController.php
+++ app/Http/Controllers/Transaction/TransactionProductController.php
@@ -343,9 +343,9 @@ class TransactionProductController extends Controller
             ); // margin footer
             $mpdf->WriteHTML($cetak);
             // Output a PDF file directly to the browser
-           if (ob_get_level() > 0) {
-                ob_end_clean();
-            }
+        //    if (ob_get_level() > 0) {
+        //         ob_end_clean();
+        //     }
             $mpdf->Output('Report Wo'.'('.date('Y-m-d').').pdf', 'I');
 
     }
